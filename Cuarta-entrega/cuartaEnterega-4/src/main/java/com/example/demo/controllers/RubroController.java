e150c57c431b5166eea9e5c7b4d34758481bf42f
Lucas Alegre <lalegre@asjservicios.com>
2024-01-29 12:34:42 -0300

    get, post, put and deleto to rubro

--- Cuarta-entrega/cuartaEnterega-4/src/main/java/com/example/demo/controllers/RubroController.java
+++ Cuarta-entrega/cuartaEnterega-4/src/main/java/com/example/demo/controllers/RubroController.java
@@ -1,28 +1,53 @@
 package com.example.demo.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.models.Categoria;
 import com.example.demo.models.Rubro;
 import com.example.demo.services.RubroService;
-
 
 @RestController
 @RequestMapping("/rubro")
 public class RubroController {
-	
+
 	@Autowired
 	RubroService rubroService;
 
 	@GetMapping()
 	public ResponseEntity<List<Rubro>> getRubro() {
 		return ResponseEntity.ok(rubroService.obtenerRubro());
-    }
-	
-	
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Optional<Rubro>> getById(@PathVariable Integer id) {
+		return ResponseEntity.ok(rubroService.findById(id));
+	}
+
+	@PostMapping("")
+	public ResponseEntity<String> createRubro(@RequestBody Rubro rubro) {
+		return ResponseEntity.ok(rubroService.cearRubro(rubro));
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<String> updateRubro(@PathVariable int id, @RequestBody Rubro rubro) {
+		return ResponseEntity.ok(rubroService.modificarRubro(id, rubro));
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Rubro> deleteRubro(@PathVariable int id) {
+		return ResponseEntity.ok(rubroService.eliminarRubro(id));
+	}
+
 }
--- Cuarta-entrega/cuartaEnterega-4/src/main/java/com/example/demo/services/RubroService.java
+++ Cuarta-entrega/cuartaEnterega-4/src/main/java/com/example/demo/services/RubroService.java
@@ -1,21 +1,52 @@
 package com.example.demo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
 import com.example.demo.models.Rubro;
 import com.example.demo.repositories.RubroRepository;
+
 @Service
 public class RubroService {
-	
+
 	@Autowired
 	RubroRepository rubroRepository;
 
 	public List<Rubro> obtenerRubro() {
 		return rubroRepository.findAll();
 	}
-	
+
+	public Optional<Rubro> findById(Integer id) {
+		return rubroRepository.findById(id);
+	}
+
+	public String cearRubro(Rubro rubro) {
+		rubroRepository.save(rubro);
+		return "Rubro creado correctamente";
+	}
+
+	public String modificarRubro(Integer id, Rubro rubro) {
+		try {
+			Rubro r = rubroRepository.findById(id).get();
+			r.setNombre(rubro.getNombre());
+			rubroRepository.save(r);
+			return "Rubro " + id + " modificado correctamente.";
+		} catch (Exception err) {
+			return "Error: El rubro no pudo ser modificado.";
+		}
+	}
+
+	public Rubro eliminarRubro(Integer id) {
+		try {
+			Rubro rubroToReturn = rubroRepository.findById(id).get();
+			rubroRepository.deleteById(id);
+			return rubroToReturn;
+		} catch (Exception err) {
+			err.getMessage();
+		}
+		return null;
+	}
 }
