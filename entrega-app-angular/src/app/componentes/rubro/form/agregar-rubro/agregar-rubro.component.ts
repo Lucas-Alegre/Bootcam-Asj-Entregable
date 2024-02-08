cf42dc5a111e55c3050f86bd67eb79e042d07f21
Lucas Alegre <lalegre@asjservicios.com>
2024-02-08 09:35:46 -0300

    Se realiza validacion de formulario enviar rubros

--- entrega-app-angular/src/app/componentes/rubro/form/agregar-rubro/agregar-rubro.component.ts
+++ entrega-app-angular/src/app/componentes/rubro/form/agregar-rubro/agregar-rubro.component.ts
@@ -12,6 +12,8 @@ export class AgregarRubroComponent {
 
   nombre: String = "";
   private modalService = inject(NgbModal);
+  nombreInvalido: boolean = false;
+  editarInvalido: boolean = false;
 
   constructor(private rubroServices: RubroService, private route: Router) { }
 
@@ -19,17 +21,33 @@ export class AgregarRubroComponent {
     this.modalService.open(longContent, { scrollable: true });
   }
 
+  nombreChange(evento: any) {
+    if (evento.length < 4) {
+      this.nombreInvalido = true
+    } else {
+      this.nombreInvalido = false
+      this.editarInvalido = false;
+    }
+  }
+
   enviar(form: any, longContent: TemplateRef<any>): void {
     const newRubro = {
       nombre: this.nombre,
       habilitado: 1,
     }
-    this.openScrollableContent(longContent)
-    this.rubroServices.post(newRubro).subscribe((data: any) => {
-      console.log(data)
-    }, (error) => {
-      this.route.navigate(['/', 'rubros'])
-    });
+    if (this.nombre.length >= 4) {
+      this.openScrollableContent(longContent)
+      this.rubroServices.post(newRubro).subscribe((data: any) => {
+        console.log(data)
+      }, (error) => {
+        this.route.navigate(['/', 'rubros'])
+      });
+    } else {
+      this.nombreInvalido = true
+      this.editarInvalido = true;
+    }
+
+
   }
 
   limpiar() {
