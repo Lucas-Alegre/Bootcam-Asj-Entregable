ce87203227cf8e99ab9e7c694c260a13537b7581
Lucas Alegre <lalegre@asjservicios.com>
2024-02-09 12:30:40 -0300

    se cambia la base de datos

---
+++ Cuarta-entrega/cuartaEnterega-4/src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO pais (nombre) VALUES
+('Argentina'), 
+('Uruguay'), 
+('Brasil');
+
+INSERT INTO PROVINCIA (nombre, pais) VALUES
+('Entre Ríos',1),
+('Córdoba',1),
+('Montevideo',2),
+('Maldonado',2),
+('Río de Janeiro',3),
+('Pará',3);
